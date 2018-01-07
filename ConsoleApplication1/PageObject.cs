742d7c70604c28daf52559cad2a034c156c8b274
Peta <Peta@DESKTOP-U0B6KFN>
2018-01-07 23:58:34 +0100

    Add project files.

---
+++ ConsoleApplication1/PageObject.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleApplication1
+{
+    public class PageObject
+    {
+
+        private String header;
+        private bool isClicked = true;
+        private IWebElement getLink;
+        private int a = 1;
+        private List<string> clickedLinksList = new List<string> { "init" };
+        private int counter = 0;
+        public IWebDriver webDriver = new ChromeDriver();
+
+        public void OpenRandomWikiPage()
+        {
+            webDriver.Url = "https://en.wikipedia.org/wiki/Special:Random";
+        }
+
+        public String GetHeader()
+        {
+            return header;
+        }
+
+        public int GetCounter()
+        {
+            return counter;
+        }
+
+        public void CheckArticleHeader()
+        {
+            try
+            {
+                IWebElement checkHeader = webDriver.FindElement(By.Id("firstHeading"));
+                header = checkHeader.Text;
+            }
+            catch (NoSuchElementException)
+            {
+            }
+        }
+   
+        public void CheckLinkIfClicked()
+        {
+            foreach (string clicked in clickedLinksList)
+            {
+                if (clicked == getLink.Text)
+                {
+                    a++;
+                    isClicked = true;
+                    break;
+                }
+                if (clicked != getLink.Text)
+                {
+                    isClicked = false;
+                }
+            }
+        }
+
+        public void ClickLink()
+        {
+            clickedLinksList.Add(getLink.Text);
+            getLink.Click();
+            isClicked = true;
+            a = 1;
+            counter++;
+        }
+        public void GetValidLink()
+        {
+            while (isClicked)
+            {
+                try
+                {
+                    getLink = webDriver.FindElement(By.XPath("//*[@id='mw-content-text']/div/p[1]/a[" + a + "]"));
+                }
+                catch (NoSuchElementException)
+                {
+                    TrySecondParagraph();
+                    return;
+                }
+                CheckLinkIfClicked();
+            }
+            getLink = webDriver.FindElement(By.XPath("//*[@id='mw-content-text']/div/p[1]/a[" + a + "]"));
+            ClickLink();
+        }
+
+        public void TrySecondParagraph()
+        {
+            while (isClicked)
+            {
+                try
+                {
+                    getLink = webDriver.FindElement(By.XPath("//*[@id='mw-content-text']/div/p[2]/a[" + a + "]"));
+                }
+                catch (NoSuchElementException)
+                {
+                    TryAnotherLink();
+                    return;
+                }
+                CheckLinkIfClicked();
+            }
+            getLink = webDriver.FindElement(By.XPath("//*[@id='mw-content-text']/div/p[2]/a[" + a + "]"));
+            ClickLink();
+        }
+
+        public void TryAnotherLink()
+        {
+            try
+            {
+                getLink = webDriver.FindElement(By.XPath("//*[@id='mw-content-text']/div/p[1]/b/a"));
+                ClickLink();
+            }
+            catch (NoSuchElementException)
+            {
+                webDriver.Url = "https://en.wikipedia.org/wiki/Special:Random";
+            }
+        }
+
+        public void Reset()
+        {
+            header = "";
+            counter = 0;
+            clickedLinksList = new List<string> { "init" };
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using ConsoleApplication1;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+
+
+
+namespace Philosophy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            
+ 
+            System.Console.WriteLine("Please choose a number of cycles:");
+            System.Int32.TryParse(System.Console.ReadLine(), out userInput);
+
+            while (true) { 
+
+            if (userInput > 0)
+            {
+
+                 PageObject pageObject = new PageObject();
+
+                for (var i = 1; i <= userInput; i++)
+                {
+
+                    pageObject.OpenRandomWikiPage();
+
+                    while (pageObject.GetHeader() != "Philosophy")
+                    {
+                        pageObject.CheckArticleHeader();
+                        if (pageObject.GetHeader() != "Philosophy")
+                        {
+                            pageObject.GetValidLink();
+                        }
+                    }
+                    System.Console.WriteLine("Number of steps needed in {0}. cycle: {1}", i, pageObject.GetCounter());
+                    pageObject.Reset();
+                }
+
+                    System.Console.WriteLine("Feel free to try again, choose a number of cycles:");
+                    System.Int32.TryParse(System.Console.ReadLine(), out userInput);
+
+                }
+            else
+            {
+                System.Console.WriteLine("Invalid choice, please try again:");
+                    System.Int32.TryParse(System.Console.ReadLine(), out userInput);
+                }
+        }
+        }
+    }
+}
